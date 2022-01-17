9f14208821a29dee4e719507413abc318f0abacb
GaelicGrime <will.angus.blaylock@gmail.com>
2022-01-17 08:30:52 -0700

    Updated USGS_C.cpython-310.pycstack.json test.txt .

--- doIt.py
+++ doIt.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env /usr/bin/python
 
 
+
+from CF.SUBM_D import _00_DEBUG as DBG
 from earthquakeSucker import USGS_C
 
-with USGS_C.USGS_C() as GGJ:
-  GGJ.getAFile()
-  GGJ.parseAFile()
+
+def main():
+  with USGS_C.USGS_C() as GGJ:
+    GGJ.getAFile()
+    GGJ.parseAFile()
+
+def cherry():
+  print(f"""\n\nI am {DBG.whoAmI()}""")
+
+def chapstick():
+  cherry()
+
+cherry()
+chapstick()
